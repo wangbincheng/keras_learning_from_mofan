31d364178c90bc7c5f86b3f9e3901183801598dd
wangbincheng <359477586@qq.com>
2019-04-28 17:08:40 +0800

    Create Save & reload 保存提取.py

---
+++ Save & reload 保存提取.py
@@ -0,0 +1,43 @@
+
+# please note, all tutorial code are running under python3.5.
+# If you use the version like python2.7, please modify the code accordingly
+
+# 10 - save
+
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.models import load_model
+
+# create some data
+X = np.linspace(-1, 1, 200)
+np.random.shuffle(X)    # randomize the data
+Y = 0.5 * X + 2 + np.random.normal(0, 0.05, (200, ))
+X_train, Y_train = X[:160], Y[:160]     # first 160 data points
+X_test, Y_test = X[160:], Y[160:]       # last 40 data points
+model = Sequential()
+model.add(Dense(output_dim=1, input_dim=1))
+model.compile(loss='mse', optimizer='sgd')
+for step in range(301):
+    cost = model.train_on_batch(X_train, Y_train)
+
+# save保存模型
+print('test before save: ', model.predict(X_test[0:2]))
+model.save('my_model.h5')   # HDF5 file, you have to pip3 install h5py if don't have it
+del model  # deletes the existing model
+
+# load导入模型并应用
+model = load_model('my_model.h5')
+print('test after load: ', model.predict(X_test[0:2]))
+
+'''
+# save and load weights,只保存权重而不保存模型的结构
+model.save_weights('my_model_weights.h5')
+model.load_weights('my_model_weights.h5')
+# save and load fresh network without trained weights
+from keras.models import model_from_json
+json_string = model.to_json()
+model = model_from_json(json_string)
+'''
